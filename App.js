51c0ade5e206e40d8c6a212ac5a84915ad8d8a4e
Christian Martins <christian@labdynamo.com>
2020-02-26 22:20:01 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title="MarketPlace Tracker" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  }
+});
